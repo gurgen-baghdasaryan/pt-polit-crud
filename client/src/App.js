f363f79890b9c6a9cbeec0e05a21e179cc22adca
gugohh <oohhgug@gmail.com>
2022-03-12 13:40:30 +0100

    added bootstrap and reacts-routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+
 import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+import PoliticList from "./components/PoliticList"
+import CreatePolitic from "./components/CreatePolitic"
+import Politic from "./components/Politic"
 
 function App() {
   return (
-   <div className="App">
-    <h2>Hello there</h2>
-   </div>
+    <div>
+    <NavBar />
+    <div className="container p-4">
+      <Routes>
+        <Route exact path="/" element={<PoliticList />} />
+        <Route exact path="/CreateProduct" element={<CreatePolitic />} />
+        <Route exact path="/edit/:id" element={<CreatePolitic />} />
+        <Route exact path="/product/:id" element={<Politic />} />
+      </Routes>
+    </div>
+  </div>
   );
 }
 
