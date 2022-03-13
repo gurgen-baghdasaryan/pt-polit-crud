8b309aeef8d2d7f9f57c0d62b413d48408ecc444
gugohh <oohhgug@gmail.com>
2022-03-13 17:32:07 +0100

    Error handling adding new route and component Error

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand" to="/">
-          Politica
+          Políticos
         </Link>
         <button
           className="navbar-toggler"
